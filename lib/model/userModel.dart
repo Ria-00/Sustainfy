0d032e67789b2668e25ff99687101d3c454064d4
Riya Singh <singhriyaa006@gmail.com>
2025-01-26 18:46:28 +0530

    fixed version issues

--- lib/model/userModel.dart
+++ lib/model/userModel.dart
@@ -1,9 +1,47 @@
-class User {
-  final String userName;
-  final String userProfilePhoto;
+class UserClass {
+  String? userName;
+   String? usermail;
+   String? password;
+   int? phone;
+   String? address;
+   Map<dynamic,dynamic>? cart;
+   Map<dynamic,dynamic>? orders;
+   String? userProfilePhoto;
 
-  User({
-    required this.userName,
-    required this.userProfilePhoto,
+
+
+
+  UserClass({
+    required this.usermail,
+    required this.password,
   });
+
+  UserClass.withPhoto({
+    required this.userName,
+    required this.userProfilePhoto
+  });
+
+  Map<String,dynamic> toMap(){
+    return(
+      {
+        "userName":userName,
+        "email":usermail,
+        "password":password,
+        "phone":phone,
+        "address":address,
+        "cart":cart,
+        "orders":orders
+
+      }
+    );
+  }
+
+  get getusermail => this.usermail;
+
+ set setusermail( usermail) => this.usermail = usermail;
+
+  get getPassword => this.password;
+
+ set setPassword( password) => this.password = password;
+ 
 }
