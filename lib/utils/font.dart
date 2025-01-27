f389f8a6466470c86c1bda66e92ee40ea92dd87d
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2025-01-27 21:56:11 +0530

    settings page: support and reset password modal sheet added

--- lib/utils/font.dart
+++ lib/utils/font.dart
@@ -3,12 +3,19 @@ import 'dart:ui';
 class AppFonts {
   // Font Family
   static const String inter = 'Inter';
+  static const String jost = 'Jost';
 
   // Font Sizes
   static const double interRegular18 = 18.0;
   static const double interSemiBold20 = 20.0;
 
+  static const double jostRegular18 = 18.0;
+  static const double jostSemiBold20 = 20.0;
+
   // Font Weights
   static const FontWeight interRegularWeight = FontWeight.w400;
   static const FontWeight interSemiBoldWeight = FontWeight.w600;
+
+  static const FontWeight jostRegularWeight = FontWeight.w400;
+  static const FontWeight jostSemiBoldWeight = FontWeight.w600;
 }
